bba97d02a2d8be3ea29e61014ee4a7ea6511c2c7
karolpasierb <lefanu@vivaldi.net>
2018-03-25 20:12:44 +0100

    Completed solution to problem 2

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Euler_Project_Solutions_Csharp
             
             Console.WriteLine("Euler project 1 solution is " + MultiplesOf3And5());
             Console.WriteLine("Euler project 2 solution is " + EvenFibbonacciNumbers());
-
+            Console.WriteLine("Euler project 3 solution is " + LargestPrimeFactor());
 
 
             Console.ReadKey();
@@ -35,13 +35,12 @@ namespace Euler_Project_Solutions_Csharp
         public static long EvenFibbonacciNumbers()
         {
             long solution = 0;
-            long[] fibbonacci = new long[5000000];
+            long[] fibbonacci = new long[5000];
             fibbonacci[0] = 1;
             fibbonacci[1] = 1;
-            for (int i = 2; fibbonacci[i] < 4000000; i++)
+            for (int i = 2; solution < 4000000; i++)
             {
                 fibbonacci[i] = fibbonacci[i-1] + fibbonacci[i - 2];
-                Console.Write(fibbonacci[i] + ", ");
                 if (fibbonacci[i]%2 == 0)
                 {
                     solution += fibbonacci[i];
@@ -50,6 +49,27 @@ namespace Euler_Project_Solutions_Csharp
             return solution;
         }
 
+        public static long LargestPrimeFactor()
+        {
+            long solution = 600851475142;
+
+            long prime = 600851475143;
+
+            while (solution >3)
+            {
+                if (prime%solution==0 && solution%2!=0)
+                {
+                    break;
+                }
+                else
+                {
+                    solution--;
+                }
+
+            }
+
+            return solution;
+        }
 
         public static int Template()
         {
